93dbe20c91259a2dd32f6621d2d3adcb041580d8
Yuvaraj <droptoyuva89@gmail.com>
2020-12-02 16:09:04 +0530

    Changes in MyUtils Class

--- src/test/java/com/learning/junit/testing/MyUtils.java
+++ src/test/java/com/learning/junit/testing/MyUtils.java
@@ -1,7 +1,14 @@
 package com.learning.junit.testing;
 
+
 public class MyUtils {
-	
+
+	/**
+	 * Simple Util Class
+	 * @param x
+	 * @param y
+	 * @return
+	 */
 	public static int add(int x, int y) {
 		return x+y;
 	}
